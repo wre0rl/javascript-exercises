d7255247e3fba71a63f6c6a9a0f57c6d8d8804dc
wre0rl <wre0rl@gmail.com>
2023-05-14 10:13:52 +0700

    set a return error for negative number input

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function(str, x) {
   // Repeat str, x times
+  if (x < 0) {
+    return 'ERROR';
+  }
+
   let combined = '';
   for (let i = 0; i < x; i++) {
     combined += str;
