fae5ba3938d5e9842c0ab58ef45659431c676520
wre0rl <wre0rl@gmail.com>
2023-05-14 13:29:29 +0700

    Removed the false comment logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,4 @@
 const leapYears = function(year) {
-  /*
-    if year % 4 = 0 && year % 100 = 0 true && year % 400 = 0, leap year return true
-    else, not leap year return false
-  */
-
   if (year % 4 === 0 && year % 100 !== 0) {
     return true;
   } else if (year % 100 === 0 && year % 400 === 0) {
