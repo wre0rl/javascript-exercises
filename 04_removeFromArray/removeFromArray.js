94e60b785b2f18e0d393a68569c0495927895fe1
wre0rl <wre0rl@gmail.com>
2023-05-14 12:13:24 +0700

    Using indexOf instead of x directly to match the removed index from the array so it doesnt skip over when looped

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...index) {
+  const listOfIndex = [...index];
+  const result = [...arr];
 
+  for (let i = 0; i < listOfIndex.length; i++) {
+    for (let x = 0; x < arr.length; x++) {
+      if(listOfIndex[i] === arr[x]) {
+        result.splice(result.indexOf(arr[x]), 1);
+      }
+    }
+  }
+
+  return result;
 };
+
+//console.log(removeFromArray([1, 2, 3], "1", 3));
+//console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3));
+console.log(removeFromArray(["a", "b", "c", "d"], "a", "b"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
