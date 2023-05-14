4e15bcfc6418f095f0e3389ea3781478661e4ed9
wre0rl <wre0rl@gmail.com>
2023-05-14 13:28:50 +0700

    Completed the solution for leap years by adding if else &&  and unskip the test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  /*
+    if year % 4 = 0 && year % 100 = 0 true && year % 400 = 0, leap year return true
+    else, not leap year return false
+  */
 
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  } else if (year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(leapYears(1800));
 
 // Do not edit below this line
 module.exports = leapYears;
