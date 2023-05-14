16f05ac75b8c5eafb9b12b64a48f21ba58b6e422
wre0rl <wre0rl@gmail.com>
2023-05-14 13:43:59 +0700

    updated the formula and adding a function for rounding the decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const formula = (f - 32) * 5/9;
+  return round(formula);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const formula = (c * 9/5) + 32;
+  return round(formula);
 };
+
+function round(num) {
+  return Math.round(num * 10) / 10
+}
 
 // Do not edit below this line
 module.exports = {
