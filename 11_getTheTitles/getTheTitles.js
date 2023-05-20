1bcbfd96802f60c351b0e7b08d824dd3ab42e6ec
wre0rl <wre0rl@gmail.com>
2023-05-20 15:27:58 +0700

    Updated the function using .map method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  return books.map((book) => book.title);
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
