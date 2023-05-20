3f78f1baeef02576fb13debd0f1c39ce67ba36df
wre0rl <wre0rl@gmail.com>
2023-05-20 14:52:06 +0700

    Palindromes solved with an ugly way

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let res = str.split('!').join('').split(',').join('').split('.').join('').split(' ').join('');
+  res = res.toLowerCase();
 
+  if (res === res.split('').reverse().join('')) {
+    return true;
+  }
+
+  return false;
 };
 
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
+
+/*
+function reverseString(str) {
+  let removeComma = str.split(',');
+  let toString = removeComma.join('');
+  let removeDot = toString.split('.');
+  let toString2 = removeDot.join('');
+  let removeSpace = toString2.split(' ');
+  let result = removeSpace.join('');
+  return result.reverse();
+
+  //let split = str.split(',');
+  //let reverseArray = split.reverse();
+  
+  //return reverseArray.join('');
+}
+
+console.log(reverseString('A car, a man, a maraca.'));
+*/
 // Do not edit below this line
 module.exports = palindromes;
