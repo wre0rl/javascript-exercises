c9fab446df0576b92c333387111f3ea127725e23
wre0rl <wre0rl@gmail.com>
2023-05-20 15:24:34 +0700

    Updated fibonacci code the ugly way

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,35 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  index = parseInt(index);
+  if (index < 0) {
+    return 'OOPS';
+  }
 
+  let previousValue = 0;
+  let nextValue = 1;
+  let sum = 1;
+
+  const fib = [];
+  for (let i = 0; i < 25; i++) {
+    fib.push(sum);
+    sum = previousValue + nextValue;
+    previousValue = nextValue;
+    nextValue = sum;
+  }
+
+  const result = fib[index-1];
+  return result;
 };
+
+/*
+0 1 1
+1 1 2
+1 2 3
+2 3 5
+3 5 8
+5 8 13
+*/
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
