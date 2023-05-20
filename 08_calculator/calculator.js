ad3f9718dc1308af6362b7bc7ee5e4d3426b4b9e
wre0rl <wre0rl@gmail.com>
2023-05-20 14:08:08 +0700

    Completed exercises 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((previousValue, currentValue) => previousValue * currentValue);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let sum = 1;
+	for (let i = x; i >= 1; i--) {
+    sum = i * sum;
+  }
+  return sum;
 };
+
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
